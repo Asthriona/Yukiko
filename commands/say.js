92e5ea7d361525dc02658a4927a13fefd26f7daf
Asthriona <im@asthriona.com>
2019-11-23 08:01:16 +0100

    Create Say

---
+++ commands/say.js
@@ -0,0 +1,20 @@
+var discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args) => {
+    //say
+    console.log(`${message.author.username} used !say`)
+    if(message.author.id === "186195458182479874"){ 
+    let botmessage = args.join(" ");
+    message.delete().catch();
+    console.log(`${message.author.username} -> ${botmessage}`)
+    message.channel.send(botmessage);
+}else{
+    return message.reply("No.");
+}
+}
+
+module.exports.help = {
+    name: "say",
+    description: "THE WALL SAID SOMETHING! YOU BETTER LISTEN!"
+}
