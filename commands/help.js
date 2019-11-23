8d26abbcfb309ac81523f0bf1320542772eb45d5
Asthriona <im@asthriona.com>
2019-11-23 08:32:59 +0100

    update help

--- commands/help.js
+++ commands/help.js
@@ -5,17 +5,18 @@ let p = "a!"
 let bicon = 
 
 module.exports.run = async (bot, message, args) => {
-    let  embed = new discord.RichEmbed()
-    .setColor("#800080")
-    .setTitle("Commands")
-    .setDescription("La liste de toutes les commands disponible.")
-    .setAuthor("Asthriona Moderation bot", bot.user.displayAvatarURL, 'https://Asthriona.com')
-    .addField(`${p}help-Game`, "Afficher la liste des jeux.")
-    .addField(`${p}help-mod`, "affiche la liste des commands des moderateur.")
-    .addField(`${p}help-commands`, "affiche la liste des commands général.")
-    .setFooter('clique ici pour une liste complete des commandes.', bot.user.displayAvatarURL)
-
-    message.channel.send(embed)
+//    let  embed = new discord.RichEmbed()
+//    .setColor("#800080")
+//    .setTitle("Commands")
+//    .setDescription("La liste de toutes les commands disponible.")
+//    .setAuthor("Asthriona Moderation bot", bot.user.displayAvatarURL, 'https://Asthriona.com')
+//    .addField(`${p}help-Game`, "Afficher la liste des jeux.")
+//    .addField(`${p}help-mod`, "affiche la liste des commands des moderateur.")
+//    .addField(`${p}help-commands`, "affiche la liste des commands général.")
+//    .setFooter('clique ici pour une liste complete des commandes.', bot.user.displayAvatarURL)
+//
+//    message.channel.send(embed)
+message.reply("https://github.com/Asthriona/AsthriModBot/blob/master/README.md")
 }
 
 module.exports.help = {
