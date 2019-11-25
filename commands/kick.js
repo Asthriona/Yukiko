524686773985660f55e23b0a1ebe0887c63006f3
Asthriona <im@asthriona.com>
2019-11-25 15:03:00 +0100

    Fix error on kick withour reason

--- commands/kick.js
+++ commands/kick.js
@@ -8,7 +8,8 @@ module.exports.run = async (bot, message, args) => {
     let kUser  = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return message.channel.send("Utilisateur Introvable D:");
     let kReason = args.join(" ").slice(22);
-    if(kUser.hasPermission('KICK_MEMBERS', false, false)) return message.channel.send("Tu peut pas On peut pas kicker les Master!")
+    if(!kReason) return message.reply("Hey! Ta oublier la raison! :O")
+    if(kUser.hasPermission('KICK_MEMBERS', false, false)) return message.channel.send("On peut pas kicker le Master!")
     if(message.member.hasPermission('KICK_MEMBERS', false, false)){
 
     let embed = new discord.RichEmbed()
@@ -22,16 +23,18 @@ module.exports.run = async (bot, message, args) => {
 
     let incidentChannel = message.guild.channels.find('name', "incident");
     if(!incidentChannel) return message.channel.send("Oops Erreur 10-Kicknf :/ <@186195458182479874> ");
-
+    
+    let bicon = bot.user.displayAvatarURL;
     let publicKick = new discord.RichEmbed()
     .setDescription(`${kUser} got 10-Ricco'd! for **${kReason}***`)
     .setImage("https://cdn.asthriona.com/kick.gif")
+    .setFooter("Kick Sponsored by Asthriona LLC!" , `${bicon}`, "https://Asthriona.Com")
     message.channel.send(publicKick)
     incidentChannel.send(embed)
-    message.guild.member(kUser).kick(kReason);
+    message.guild.member(kUser).kick(`${kReason} ${message.author}`);
 
 
-}else{ return message.channel.send("hé! Ta pas le droit de 10-ricco des gens toi!");
+}else{ return message.channel.send("Oy! Ta pas le droit de 10-ricco des gens toi!");
 };
 }
 
