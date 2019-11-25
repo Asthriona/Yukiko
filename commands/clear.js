5d8829b196002238efecd1e75306c95f29c0af2a
Asthriona <im@asthriona.com>
2019-11-25 14:37:53 +0100

    Traduction FR a!clear

--- commands/clear.js
+++ commands/clear.js
@@ -2,8 +2,8 @@ var discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     //clear
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-    if(!args[0]) return message.channel.send("oof");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Oy! Tu delete pas ok?! C'est pas sympas de delete les messages des autres!");
+    if(!args[0]) return message.channel.send("Oy! Ta pas mis de chiffres!");
     message.channel.bulkDelete(args[0]).then(() => {
         message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(1000));
     });
