2d5bb3367f01bb48ce6621c45a69e436cccc7849
Asthriona <im@asthriona.com>
2019-12-04 16:36:26 +0100

    add typing to say.js

--- commands/say.js
+++ commands/say.js
@@ -3,14 +3,20 @@ var discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     //say
+    message.channel.startTyping()
     console.log(`${message.author.username} used !say`)
     if(message.author.id === "186195458182479874"){ 
     let botmessage = args.join(" ");
     message.delete().catch();
     console.log(`${message.author.username} -> ${botmessage}`)
-    message.channel.send(botmessage);
+    setTimeout(function(){ 
+        message.channel.stopTyping();
+        message.channel.send(botmessage);
+    }, 3000);
 }else{
-    return message.reply("Oy! t'as pas le droit de me dire ce que je doit dire! :'c");
+    setTimeout(function(){ 
+        return message.reply("Oy! t'as pas le droit de me dire ce que je doit dire! :'c");
+    }, 4000);
 }
 }
 
@@ -18,3 +24,4 @@ module.exports.help = {
     name: "say",
     description: "THE WALL SAID SOMETHING! YOU BETTER LISTEN!"
 }
+
