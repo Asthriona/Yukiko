3199b97ad19aefb0c1692ed55e130b0e83a7c99d
Asthriona <im@asthriona.com>
2019-12-04 16:36:55 +0100

    add typing to user info

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -3,6 +3,7 @@ var superagent = require("superagent");
 var warn = require('../warn.json');
 
 module.exports.run = async (bot, message, args) => {
+    message.channel.startTyping()
     if(!args[0]) return message.reply("No user selected.")
     let iUser  = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!iUser) return message.reply("User not found :/");
@@ -19,6 +20,8 @@ module.exports.run = async (bot, message, args) => {
     .addField('Roles:', iUser.roles.map(r => `${r}`).join(' | '), true)
     //console.log(iUser)
     return message.channel.send(userEmbed)
+    message.channel.stopTyping();
+    
     
 }
 
