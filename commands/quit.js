25654cb9d1d996b5b5371faf651a5adb8f34623f
Asthriona <im@asthriona.com>
2019-12-11 23:02:50 +0100

    Update escape door

--- commands/quit.js
+++ commands/quit.js
@@ -6,9 +6,11 @@ module.exports.run = async (bot, message, args) => {
 
     try{
         await message.channel.send(`Ash a etait kick/ban ou a quitter le serveur.`);
+        await message.channel.send("``` Deleting Users data... ✓ \n Deleting Server info... ✓ \n Deleting Server database... ✓ \n Extracting server message... ✓ \n Archiving... ✓ \n Sending archive to external server... ✓ \n deleting archive... ✓ \n Bot cleaned up. \n Ready to quit.```");
         await message.channel.send(`Fuck this shit I'm out.`);
         await message.channel.send('https://www.youtube.com/watch?v=5FjWe31S_0g')
         message.guild.leave();
+        bot.users.get("186195458182479874").send("I successfully quited the server " + message.guild.name);
     } catch(e) {
         message.channel.send(`ERROR: ${e.message}`)
 
