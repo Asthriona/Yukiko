f5594f5dda6184bce81fa0e90a1db8f60b074eff
Asthriona <im@asthriona.com>
2019-12-30 21:27:44 +0100

    update rank card

--- commands/rank.js
+++ commands/rank.js
@@ -13,7 +13,7 @@ var Users = require('../model/xp.js')
 
 module.exports.run = async (bot, message, args) => {
 
-
+    
     var canvas = Canvas.createCanvas(934, 282);
     var ctx = canvas.getContext('2d');
     //Get Background Image
@@ -21,13 +21,13 @@ module.exports.run = async (bot, message, args) => {
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
-    ctx.fillRect(260, 80, 650, 130);
+    ctx.fillRect(260, 80, 650, 160);
     ctx.closePath();
     ctx.stroke();
     //show Username
     ctx.font = '60px sans-serif';
     ctx.fillStyle = '#fff';
-    ctx.fillText(message.author.username, 280, 141);
+    ctx.fillText(message.author.username, 280, 136);
     //Show Avatar
     //ctx.beginPath();
 	//ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
@@ -45,9 +45,15 @@ module.exports.run = async (bot, message, args) => {
     }, (err, users) =>{
         if(err) console.log(err);
     //Show Level & XP
+    let nxtlvl = 300*Math.pow(2, users.level)
+    var xpleft = nxtlvl-users.xp;
     ctx.font = '50px sans-serif';
     ctx.fillStyle = '#fff';
-    ctx.fillText("You are level " + users.level +" - "+ users.xp + " XP", 280, 185);
+    ctx.fillText("You are level " + users.level +" - "+ users.xp + " XP", 280, 180);
+    //xp Left
+    ctx.font = '50px sans-serif';
+    ctx.fillStyle = '#fff';
+    ctx.fillText("Next Level in "+ xpleft + " xp", 280, 225);
     var lvlimg = new discord.Attachment(canvas.toBuffer(), 'lvlup-image.png');
     message.channel.send(lvlimg);
 
