63e98d2bcc763ae870d009d690d4e7c2a03316d7
Asthriona <im@asthriona.com>
2020-01-12 14:27:00 +0100

    update wow API now works with realm and region

--- commands/wow.js
+++ commands/wow.js
@@ -4,25 +4,47 @@ var axios = require('axios');
 var botConfig = require('../botconfig.json')
 
 module.exports.run = async (bot, message, args) => {
-  var char = args.join(" ");
-  axios.get('https://eu.api.blizzard.com/wow/character/magtheridon/'+char+'?locale=en_US&access_token='+botConfig.wowAT).then(function(res) {
+  let char = args[0];
+  let realm = args[1];
+  let region = args[2]
+  if(!region) region = "eu"
+  console.log(args[0] + " " + args[1])
+  axios.get('https://'+region+'.api.blizzard.com/wow/character/'+realm+'/'+char+'?locale=en_US&access_token='+botConfig.wowAT).then(function(res) {
   
     if(message.author.id === "186195458182479874"){
-    var wowembed = new discord.RichEmbed()
-    .setTitle("Infos for: " + args)
-    .addField("Name:", res.data.name, true)
-    .addField("Realm", res.data.realm, true)
-    .addField("Battlegroup", res.data.battlegroup, true)
-    .addField("Class:", res.data.class, true)
-    .addField("Race", res.data.race, true)
-    .addField("Gender", res.data.gender, true)
-    .addField("level", res.data.level, true)
-    .addField("Achievement Points", res.data.achievementPoints, true)
-    .addField("Calc Class", res.data.calcClass, true)
-    .addField("faction", res.data.faction, true)
-    .addField("Total Honorable Kills", res.data.totalHonorableKills, true)
-    .addField("ID", res.data.id, true)
-    message.reply(wowembed)
+    if(res.data.faction === 0){
+      var wowembed = new discord.RichEmbed()
+      .setTitle("Infos for: " + args)
+      .addField("Name:", res.data.name, true)
+      .addField("Realm", res.data.realm, true)
+      .addField("Battlegroup", res.data.battlegroup, true)
+      .addField("Class:", res.data.class, true)
+      .addField("Race", res.data.race, true)
+      .addField("Gender", res.data.gender, true)
+      .addField("level", res.data.level, true)
+      .addField("Achievement Points", res.data.achievementPoints, true)
+      .addField("Calc Class", res.data.calcClass, true)
+      .addField("faction", "Alliance", true)
+      .addField("Total Honorable Kills", res.data.totalHonorableKills, true)
+      .addField("ID", res.data.id, true)
+      message.channel.send(wowembed)
+    }else{
+      var wowembed = new discord.RichEmbed()
+      .setTitle("Infos for: " + args)
+      .addField("Name:", res.data.name, true)
+      .addField("Realm", res.data.realm, true)
+      .addField("Battlegroup", res.data.battlegroup, true)
+      .addField("Class:", res.data.class, true)
+      .addField("Race", res.data.race, true)
+      .addField("Gender", res.data.gender, true)
+      .addField("level", res.data.level, true)
+      .addField("Achievement Points", res.data.achievementPoints, true)
+      .addField("Calc Class", res.data.calcClass, true)
+      .addField("faction", "Horde", true)
+      .addField("Total Honorable Kills", res.data.totalHonorableKills, true)
+      .addField("ID", res.data.id, true)
+      message.channel.send(wowembed)
+    }
     }
 }).catch(err => message.channel.send("WAW! an error sucessfully happened! ```"+err+"```"));
 }
