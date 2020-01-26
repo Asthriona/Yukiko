afb3bcfe15f340e1a09bc28a1c6e9fe1e45f03d2
Asthriona <im@asthriona.com>
2020-01-26 04:44:31 +0100

    Fix uptime. Maybe... I hope...

--- commands/uptime.js
+++ commands/uptime.js
@@ -4,15 +4,15 @@ var superagent = require("superagent");
 module.exports.run = async (bot, message, args) => {
     //Uptime
     let totalSeconds = (bot.uptime / 1000);
-    var days = Math.floor(totalSeconds / 86400);
-    var hours = Math.floor(totalSeconds / (60*60));
+    let days = Math.floor(totalSeconds / 86400);
+    let hours = Math.floor(totalSeconds / 3600);
     totalSeconds %= 3600;
-    var minutes = Math.floor(totalSeconds % (60*60) / 60);
-    var seconds = Math.floor(totalSeconds % 60);
-    var uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
-
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = Math.floor(totalSeconds % 60);
+    let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
     message.channel.send(uptime)
     console.log(`${message.author.username} used uptime ${uptime}`)
+    console.log(process.uptime())
 }
 
 module.exports.help = {
