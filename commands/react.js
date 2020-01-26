7cbdffb14e0281d5812c19cb133394c6a79de38f
Asthriona <im@asthriona.com>
2020-01-27 00:36:21 +0100

    update react

--- commands/react.js
+++ commands/react.js
@@ -2,7 +2,8 @@ var discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
     //let emote = message.guild.emojis.find('name', "japanese_goblin")
-    message.react("👺")
+    message.react(args[0])
+    .catch(err => message.reply('```'+err+'```'));
 }
 module.exports.help = {
     name: "react"
