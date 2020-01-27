0b4cd3469428403159de63d9774b134fef57c806
Asthriona <im@asthriona.com>
2020-01-27 07:21:04 +0100

    Add whois

---
+++ commands/info/whois.js
@@ -0,0 +1,44 @@
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+var {getMember, formatDate} = require("../../function")
+
+module.exports = {
+    name: "whois",
+    aliases: ["who", "user", "info"],
+    description: "Returns user information",
+    usage: "[username | id | mention]",
+    run: (bot, message, args) => {
+        const member = getMember(message, args.join(" "));
+
+        // Member variables
+        const joined = formatDate(member.joinedAt);
+        const roles = member.roles
+            .filter(r => r.id !== message.guild.id)
+            .map(r => r).join(", ") || 'none';
+
+        // User variables
+        const created = formatDate(member.user.createdAt);
+
+        const embed = new RichEmbed()
+            .setFooter(bot.user.username, bot.user.displayAvatarURL)
+            .setThumbnail(member.user.displayAvatarURL)
+            .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+            .setAuthor(message.author.username, message.author.displayAvatarURL)
+
+            .addField('Member information:', stripIndents`**=> Display name:** ${member.displayName}
+            **=> Joined at:** ${joined}
+            **=> Roles:** ${roles}`, true)
+
+            .addField('User information:', stripIndents`**=> ID:** ${member.user.id}
+            **=> Username:** ${member.user.username}
+            **=> Tag:** ${member.user.tag}
+            **=> Created at:** ${created}`, true)
+            
+            .setTimestamp()
+
+        if (member.user.presence.game) 
+            embed.addField('Currently playing', stripIndents`**> Name:** ${member.user.presence.game.name}`);
+
+        message.channel.send(embed);
+    }
+}
