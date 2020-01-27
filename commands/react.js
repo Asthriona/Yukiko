ddbc9c4b24e7b8ea9810f08a61b556a6448a17d1
Asthriona <im@asthriona.com>
2020-01-27 01:00:42 +0100

    remove comented line react

--- commands/react.js
+++ commands/react.js
@@ -1,7 +1,6 @@
 var discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    //let emote = message.guild.emojis.find('name', "japanese_goblin")
     message.react(args[0])
     .catch(err => message.reply('```'+err+'```'));
 }
