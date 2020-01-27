df1c99b90ec265e85faad4820f6ead1a48be9f59
Asthriona <im@asthriona.com>
2020-01-27 07:20:35 +0100

    Add Function file

--- function.js
+++ function.js
@@ -1,19 +1,21 @@
-module.exports= {
-    getMember: function(message, toFind = ' '){
+module.exports = {
+    getMember: function(message, toFind = '') {
         toFind = toFind.toLowerCase();
         let target = message.guild.members.get(toFind);
-        if(!target && message.mention.members)
-            target = message.mention.members.first();
-        if(!target && toFind) {
-            target = message.member.guild.members.find(member => {
-                return member.displayName.toLowerCase().includes(toFind) || member.user.tag.toLowerCase().includes(toFind)
+        if (!target && message.mentions.members)
+            target = message.mentions.members.first();
+        if (!target && toFind) {
+            target = message.guild.members.find(member => {
+                return member.displayName.toLowerCase().includes(toFind) ||
+                member.user.tag.toLowerCase().includes(toFind)
             });
-            if(!target)
-                target = message.member;
-            return target;
-        }
+        }    
+        if (!target) 
+            target = message.member;
+            
+        return target;
     },
-    formatDate: function(date){
+    formatDate: function(date) {
         return new Intl.DateTimeFormat('en-US').format(date)
     }
 }
