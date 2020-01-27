a153f7f599b93111e40ceed356e36640d03ce29a
Asthriona <im@asthriona.com>
2020-01-27 09:30:35 +0100

    Re add bot.on and cards generation + yukikodb login

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-const { Client, RichEmbed, Collection } = require('discord.js');
-const botConfig = require('./botconfig.json')
+var { Client, RichEmbed, Collection } = require('discord.js');
+var botConfig = require('./botconfig.json');
+var fs = require("fs");
+var Canvas = require('canvas');
+var mongoose = require("mongoose");
 
-const bot = new Client({
+require('./music/server');
+
+var bot = new Client({
     disableEveryone: true
 });
 
+//YukikoDB
+mongoose.connect(botConfig.dbLink, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).catch(error => handleError(error));
+mongoose.connection.on('error', function(e) {
+    console.log('YukikoDB: Can not connect Error: ' + e);
+    var {bot} = new discord.Client({ disableEveryone: true });
+    bot.commands = new discord.Collection();
+    process.exit();
+});
+mongoose.connection.once('open', function(d) { 
+    console.log("\x1b[32mYukikoDB:\x1b[0m connected to \x1b[31m" + mongoose.connection.host + " \x1b[0m");
+})
+var Users = require('./model/xp.js')
+
 bot.commands = new Collection();
 bot.aliases = new Collection();
+bot.categories = fs.readdirSync("./commands/");categories = fs.readdirSync("./commands/");
 
 ["command"].forEach(handler => {
     require(`./handler/${handler}`)(bot);
 })
 
+bot.on('disconnect', () => console.log("\x1b[32mAsthriona Mod Bot\x1b[0m is Disconected... Waiting for reconnect"));
+bot.on('reconnecting', () => console.log("\x1b[32mAsthriona Mod Bot\x1b[0m  is reconnecting."))
+
 bot.on("ready", () => {
-    console.log(`${bot.user.username} is online.`);
+    console.log(`\x1b[32m${bot.user.username}\x1b[0m is now started and running in \x1b[31m${process.env.NODE_ENV} \x1b[0menvironement!`);
     bot.user.setPresence({
         game:{
             name: "coding 2.0!",
@@ -21,6 +46,27 @@ bot.on("ready", () => {
         }
     })
 })
+
+// Welcome and stuff
+bot.on("guildMemberAdd", async member => {
+    const channel = member.guild.channels.find(channel => channel.name === "welcome");
+    if (!channel) {
+        const channel = member.guild.channels.find(channel => channel.name === "bienvenue");
+        return await WelcomeCad(member, channel);
+    } else {
+        return await WelcomeCad(member, channel);
+    }
+});
+
+bot.on("guildMemberRemove", member => {
+    const channel = member.guild.channels.find(channel => channel.name === "welcome");
+    if (!channel) {
+        const channel = member.guild.channels.find(channel => channel.name === "bienvenue");
+        channel.send(`${member} Viens de quitter le serveur! https://cdn.asthriona.com/sad.gif`);
+    } else {
+        channel.send(`${member} Viens de quitter le serveur! https://cdn.asthriona.com/sad.gif`);
+    }
+});
 
 bot.on("message", async message =>{
     //Logging
@@ -42,4 +88,95 @@ bot.on("message", async message =>{
 
 })
 
-bot.login(botConfig.tokenDev)
+if (process.env.NODE_ENV === 'production') {
+    bot.login(botConfig.token)
+    console.log("login on discord...")
+} else {
+    bot.login(botConfig.tokenDev)
+    console.log("login on discord...")
+};
+
+
+//Cards Generation
+
+async function lvlupimg(message, users) {
+    const applyText = (canvas, text) => {
+        const ctx = canvas.getContext('2d');
+        let fontSize = 70;
+        do {
+            ctx.font = `${fontSize -= 10}px sans-serif`;
+        } while (ctx.measureText(text).width > canvas.width - 300);
+        return ctx.font;
+    };
+    var canvas = Canvas.createCanvas(934, 282);
+    var ctx = canvas.getContext('2d');
+    var background = await Canvas.loadImage('https://cdn.asthriona.com/discordbotCard.jpg');
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    //Draw rectangle
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+    ctx.fillRect(260, 80, 650, 160);
+    ctx.closePath();
+    ctx.stroke();
+    //show Username
+    ctx.font = applyText(canvas, message.author.username);
+    ctx.fillStyle = '#fff';
+    ctx.fillText(message.author.username + " Level up!", 280, 136);
+    //Show Level & XP
+    let nxtlvl = 300 * Math.pow(2, users.level);
+    var xpleft = nxtlvl - users.xp;
+    ctx.font = '40px sans-serif';
+    ctx.fillStyle = '#fff';
+    ctx.fillText("You are level now " + users.level + " - " + users.xp + " XP", 280, 180);
+    //xp Left
+    ctx.font = '50px sans-serif';
+    ctx.fillStyle = '#fff';
+    ctx.fillText("Next Level in " + xpleft + " xp", 280, 225);
+    //Get avatar
+    await GetAvatar(message, ctx);
+    //put image together and send it
+    var lvlupimg = new discord.Attachment(canvas.toBuffer(), 'lvlup-image.png');
+    message.channel.send(lvlupimg);
+}
+
+async function WelcomeCad(member, channel) {
+    const applyText = (canvas, text) => {
+        const ctx = canvas.getContext('2d');
+        let fontSize = 70;
+        do {
+            ctx.font = `${fontSize -= 10}px sans-serif`;
+        } while (ctx.measureText(text).width > canvas.width - 300);
+        return ctx.font;
+    };
+
+    var canvas = Canvas.createCanvas(934, 282);
+    var ctx = canvas.getContext('2d');
+    var background = await Canvas.loadImage('https://cdn.asthriona.com/discordbotCard.jpg');
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+    ctx.fillRect(260, 80, 650, 130);
+    ctx.stroke();
+    ctx.font =  applyText(canvas, member.user.username);
+    ctx.fillStyle = '#fff';
+    ctx.fillText(member.user.username, 280, 141);
+    ctx.font = applyText(canvas, member.guild.name);
+    ctx.fillStyle = '#fff';
+    ctx.fillText("Welcome on " + member.guild.name, 280, 185);
+    var avatar = await Canvas.loadImage(member.user.displayAvatarURL);
+    ctx.beginPath();
+    ctx.arc(140, 128, 110, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.clip();
+    ctx.drawImage(avatar, 25, 15, 256, 256);
+    var attachment = new discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+    channel.send(attachment)
+}
+async function GetAvatar(message, ctx) {
+    var avatar = await Canvas.loadImage(message.author.displayAvatarURL);
+    ctx.beginPath();
+    ctx.arc(125, 140, 100, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.clip();
+    ctx.drawImage(avatar, 25, 40, 200, 200);
+}
