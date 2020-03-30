f941de66097e9c7dd352abdab154b7994558c686
Asthriona <im@asthriona.com>
2020-03-30 11:44:50 +0200

    add instant invit

---
+++ commands/info/invit.js
@@ -0,0 +1,11 @@
+var Discord = require("discord.js")
+module.exports = {
+    name: "invit",
+    category: "info",
+    description: "Create an invitation to this server",
+    run: async (bot, message, args, RichEmbed) => {
+        message.channel.createInvite()
+  .then(invite => message.channel.send(`Here is your invitation! => \n ${invite}`))
+  .catch(console.error);
+    }
+}
