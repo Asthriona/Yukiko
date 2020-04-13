3c01747f86260e5c83d58aca17d44d47122e6f9b
Asthriona <im@asthriona.com>
2020-04-13 21:11:44 +0200

    remove react

--- commands/info/react.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "react",
-    category: "Moderation",
-    description: "(Not functional)Use this commands to create an emoji reaction to a message and give a role to anyone who click that reaction.",
-    usage: "a!react <emoji>, <rolename>",
-    run: async (bot, message, args) => {
-        message.channel.send("This function is not supported yet.")
-    }
-}
