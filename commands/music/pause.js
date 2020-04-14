ea44ff6db38b2d1d341544ada021a9caf73611c6
Asthriona <im@asthriona.com>
2020-04-14 10:01:00 +0200

    add pause  commands in command handler

---
+++ commands/music/pause.js
@@ -0,0 +1,15 @@
+var ytdl = require("ytdl-core")
+module.exports = {
+    name: "testpause",
+    category: "music",
+    description: "Pause the current video.",
+    run: async (bot, message, args, ops) => {
+        var guildIDData = ops.active.get(message.guild.id)
+        if(!guildIDData) return message.reply("Nothing in the queue for now.");
+        if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.reply("Hey, i'm busy in another channel right now. sorry!");
+            
+        if(guildIDData.dispatcher.paused) return message.reply("Video is already paused.");
+        guildIDData.dispatcher.pause();
+        return message.reply(`***${guildIDData.queue[0].songTitle}*** is now paused!`)
+    }
+}
