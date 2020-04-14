0ab68d51d00110267285b4b6386c006e5327574c
Asthriona <im@asthriona.com>
2020-04-14 10:01:09 +0200

    Add Resume  commands in command handler

---
+++ commands/music/resume.js
@@ -0,0 +1,15 @@
+var ytdl = require("ytdl-core")
+module.exports = {
+    name: "testresume",
+    category: "music",
+    description: "Resume the current video.",
+    run: async (bot, message, args, ops) => {
+        var guildIDData = ops.active.get(message.guild.id)
+        if(!guildIDData) return message.reply("Nothing in the queue for now.");
+        if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.reply("Hey, i'm busy in another channel right now. sorry!");
+            
+        if(!guildIDData.dispatcher.paused) return message.reply("Video is not paused.");
+        guildIDData.dispatcher.resume();
+        return message.reply(`***${guildIDData.queue[0].songTitle}*** is now resumed!`)
+    }
+}
