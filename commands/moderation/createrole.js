ae1a8613fc172f6989a182bea47834f860cbbccd
Asthriona <im@asthriona.com>
2020-04-20 15:20:48 +0200

    Create role

---
+++ commands/moderation/createrole.js
@@ -0,0 +1,13 @@
+var { RichEmbed } = require("discord.js");
+var { stripIndents } = require("common-tags");
+var { promptMessage } = require("../../function");
+
+module.exports = {
+    name: "createrole",
+    category: "Moderation",
+    description: "Create a new role!",
+    usage: "!createrole <role name> <role color>",
+    run: async (bot, message, args, RichEmbed) => {
+
+    }
+}
