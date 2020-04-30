1a66c2087e06aab4d28e95fa686d0c7abf632745
Asthriona <im@asthriona.com>
2020-04-30 22:25:14 +0200

    bind update command to dev

--- commands/dev/update.js
+++ commands/dev/update.js
@@ -4,8 +4,10 @@ module.exports = {
     category: "dev",
     description: "Update the bot from github and restart it",
     run: async (bot, message, args) => {
-        exec('git pull origin master', (stdout, stderr) =>{
-            message.channel.send("update: ```" + stdout + "```")
-        })
+        if (message.author.id === "186195458182479874" || "635422418424299521") {
+            exec('git pull origin master', (stdout, stderr) => {
+                message.channel.send("update: ```" + stdout + "```")
+            })
+        }
     }
 }
