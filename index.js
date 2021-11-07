bb8636d852525b6efc8dd7f79ed70dfcb3912b12
Asthriona <im@asthriona.com>
2021-11-07 23:42:38 +0100

    refactor: Lint index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*  
+/*
 /   Morning! Time to get Dirty again. I dont have to will to create a whole new client for this, but as you are still starring the project I will update it
 / 	for Discord.js v13.
 / 	I mean so it start. Might add slash commands support later.
@@ -7,13 +7,13 @@
 /	- Asthriona
 */
 
-const { Client, MessageEmbed, Collection } = require('discord.js');
-const Config = require('./botconfig.json');
+const { Client, MessageEmbed, Collection } = require("discord.js");
+const Config = require("./botconfig.json");
 const fs = require("fs");
 const mongoose = require("mongoose");
 const { Manager } = require("erela.js");
 
-const bot = new Client({ 
+const bot = new Client({
 	disableMentions: "everyone",
 	partials: ["USER", "MESSAGE", "CHANNEL", "REACTION"],
 	intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES", "GUILD_MESSAGE_REACTIONS"],
@@ -21,20 +21,17 @@ const bot = new Client({
 
 // Connection to mongoDB
 mongoose.connect(Config.dbLink, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).catch(error => handleError(error));
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}).catch(error => console.log(error));
 
-mongoose.connection.on('error', function (e) {
-    console.log('YukikoDB: Can not connect Error: ' + e);
-    var { bot } = new discord.Client({ disableMentions: 'everyone' });
-    bot.commands = new discord.Collection();
-    process.exit();
+mongoose.connection.on("error", function(e) {
+	console.log("YukikoDB: Can not connect Error: " + e);
 });
 
-mongoose.connection.once('open', function (d) {
-    console.log("\x1b[32mYukikoDB:\x1b[0m connected to \x1b[31m" + mongoose.connection.host + " \x1b[0m");
-})
+mongoose.connection.once("open", function() {
+	console.log("\x1b[32mYukikoDB:\x1b[0m connected to \x1b[31m" + mongoose.connection.host + " \x1b[0m");
+});
 
 // Config Music bot
 bot.manager = new Manager({
@@ -50,44 +47,45 @@ bot.manager = new Manager({
 		if (guild) guild.shard.send(payload);
 	},
 })
-	.on('nodeConnect', () => console.log('Connected to Lavalink'))
-	.on('nodeError', (node, error) => console.log(`Node error: ${error.message}`))
-	.on('trackStart', (player, track) => {
+	.on("nodeConnect", () => console.log("Connected to Lavalink"))
+	.on("nodeError", (node, error) => console.log(`Node error: ${error.message}`))
+	.on("trackStart", (player, track) => {
 		if (!player.voiceChannel) {
-			bot.channels.cache.get(player.textChannel).send('Seems like I\'ve been disconnected from the voice channel. I\'m destroying the player now.');
+			bot.channels.cache.get(player.textChannel).send("Seems like I've been disconnected from the voice channel. I'm destroying the player now.");
 			return player.destroy();
 		}
 		const nowPlayingEmbed = new MessageEmbed()
-			.setAuthor('Now playing', bot.user.displayAvatarURL())
+			.setAuthor("Now playing", bot.user.displayAvatarURL())
 			.setDescription(`Now playing 『${track.title}』 requested by ${track.requester}`)
-			.setImage(track.displayThumbnail('mqdefault'))
+			.setImage(track.displayThumbnail("mqdefault"))
 			.setTimestamp()
 			.setFooter(bot.user.username, bot.user.displayAvatarURL());
 		bot.channels.cache.get(player.textChannel).send(nowPlayingEmbed);
 	})
-	.on('queueEnd', (player) => {
-		bot.channels.cache.get(player.textChannel).send('Queue has ended.');
+	.on("queueEnd", (player) => {
+		bot.channels.cache.get(player.textChannel).send("Queue has ended.");
 		player.destroy();
 	});
-    bot.on("raw", (d) => bot.manager.updateVoiceState(d));
+bot.on("raw", (d) => bot.manager.updateVoiceState(d));
 
 // Command Handler
 bot.Commands = new Collection();
 bot.Aliases = new Collection();
 bot.Categories = fs.readdirSync("./commands/");
 ["command"].forEach(handler => {
-    require(`./handler/${handler}`)(bot);
-})
+	require(`./handler/${handler}`)(bot);
+});
 
 // Event Handler
-const EventFiles = fs.readdirSync('./events/').filter(file => file.endsWith('.js'));
-for (const file of EventFiles){
-    const event = require(`./events/${file}`);
-    if(event.once){
-        bot.once(event.name, (...args) => event.execute(...args, bot));
-    }else{ 
-        bot.on(event.name, (...args) => event.execute(...args, bot));
-    }
+const EventFiles = fs.readdirSync("./events/").filter(file => file.endsWith(".js"));
+for (const file of EventFiles) {
+	const event = require(`./events/${file}`);
+	if(event.once) {
+		bot.once(event.name, (...args) => event.execute(...args, bot));
+	}
+	else{
+		bot.on(event.name, (...args) => event.execute(...args, bot));
+	}
 }
 
 bot.login(Config.token);
