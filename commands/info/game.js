5b34e108894f3ab85dc2121541fb9a1620cbf453
Asthriona <im@asthriona.com>
2021-11-07 23:52:39 +0100

    chore: update status command

--- commands/info/game.js
+++ commands/info/game.js
@@ -4,13 +4,10 @@ module.exports = {
 	description: "change what bot is actually playing.",
 	aliases: ["setgame"],
 	run: async (bot, message, args) => {
-		if(message.member.hasPermission("BAN_MEMBERS")) {
-			console.log(args[0]);
-			bot.user.setStatus("pog");
-			bot.user.setPresence({ activity: { name: args.join(" ") } });
+		if(message.member.permissions.has("BAN_MEMBERS")) {
+			bot.user.setPresence({ activities: [{ name: args.join(" ") }] });
 		}
 		else{
-			console.log("Nope");
 			return message.reply("Oy! You can't tell me what to do! ");
 		}
 
