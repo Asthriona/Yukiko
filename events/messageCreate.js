46265cffed017e5290dfe5d401c58fd521711263
Asthriona <im@asthriona.com>
2021-11-07 23:48:13 +0100

    perf: remove unused function

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,9 +3,8 @@ const Config = require("../botconfig.json");
 const Users = require("../model/xp.js");
 const Cards = require("../model/card.js");
 const { lvlupimg } = require("../Cards.js");
-const moment = require("moment");
 module.exports = {
-	name: "message",
+	name: "messageCreate",
 	once: false,
 	async execute(message, bot) {
 		if(message.author.bot) return;
@@ -67,10 +66,11 @@ module.exports = {
 				newCards.save().catch(error => console.log(error));
 			}
 		});
-		const date = `${moment().format("L")} ${moment().format("LT")}`;
-		// Log
-		if (message.channel.type === "dm") return console.log(`${date} DM from -> ${message.author.username}: ${message.content}`);
-		console.log(` ${date} ${message.guild.name} -> ${message.author.username}: ${message.content}`);
+
+		// ignore types of messages or without prefix.
+		if (message.channel.type === "dm") return;
+		if(message.author.bot) return;
+		if(!prefix) return;
 
 		// Setup Prefix and args
 		const prefix = Config.prefix;
@@ -78,14 +78,6 @@ module.exports = {
 		const args = messageArray.slice(1);
 		const cmd = messageArray[0];
 
-		if(message.author.bot) return;
-		if(!prefix) return;
-
-		// Force mute.
-		if (message.member.roles.cache.find(r => r.name === "muted")) {
-			message.delete();
-			message.author.send("You are muted on " + message.guild.name);
-		}
 		// Commands Handler
 		const commandfile = bot.Commands.get(messageArray[0].slice(prefix.length));
 		if (commandfile) commandfile.run(bot, message, args, cmd);
