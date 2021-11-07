4d4930c02e979baa7b2ec1e26dccf876ae25871a
Asthriona <im@asthriona.com>
2021-11-07 23:40:32 +0100

    lint command folder

--- handler/command.js
+++ handler/command.js
@@ -2,27 +2,28 @@ const { readdirSync } = require("fs");
 
 const ascii = require("ascii-table");
 
-let table = new ascii("Commands");
+const table = new ascii("Commands");
 table.setHeading("Command", "Load status");
 
 module.exports = (bot) => {
-    readdirSync("./commands/").forEach(dir => {
-        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
-        for (let file of commands) {
-            let pull = require(`../commands/${dir}/${file}`);
-    
-            if (pull.name) {
-                bot.Commands.set(pull.name, pull);
-                table.addRow(file, '✔️ Ok！');
-            } else {
-                table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
-                continue;
-            }
-    
-            // If there's an aliases key, read the aliases.
-            if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => bot.Aliases.set(alias, pull.name));
-        }
-    });
-    // Log the table
-    console.log(table.toString());
-}
+	readdirSync("./commands/").forEach(dir => {
+		const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
+		for (const file of commands) {
+			const pull = require(`../commands/${dir}/${file}`);
+
+			if (pull.name) {
+				bot.Commands.set(pull.name, pull);
+				table.addRow(file, "✔️ Ok！");
+			}
+			else {
+				table.addRow(file, "❌  -> missing a help.name, or help.name is not a string.");
+				continue;
+			}
+
+			// If there's an aliases key, read the aliases.
+			if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => bot.Aliases.set(alias, pull.name));
+		}
+	});
+	// Log the table
+	console.log(table.toString());
+};
