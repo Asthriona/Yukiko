6c997f7658d9dcaec0dd098716ffebe9cef3adfc
Asthriona <54015295+Asthriona@users.noreply.github.com>
2022-06-08 14:12:48 +0000

    deploy slash commands

---
+++ deploy-commands.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const { clientId, guildId, token } = require("./botconfig.json");
+
+const commands = [];
+const commandPath = path.join(__dirname, "slash/commands");
+const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith(".js"));
+for(const file of commandFiles) {
+	const filePath = path.join(commandPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "9" }).setToken(token);
+(async () => {
+	try{
+		console.log("Updating commands...");
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+		console.log("Commands updated!");
+	}
+	catch(err) {
+		console.error("an error happened while trying to refresh commands the commands:");
+		console.error(err);
+	}
+})();
