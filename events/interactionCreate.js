1d32d6fc3d507cc3ee76799bc25243c7f59a0349
Makoto Kobayashi <im@asthriona.com>
2022-10-18 17:26:32 +0200

    fix: :bug: Fix Slash commands

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,14 +2,17 @@ module.exports = {
 	name: "interactionCreate",
 	once: false,
 	async execute(interaction, bot) {
+		console.log(interaction);
 		// check if command.
-		if(!interaction.isCommand()) return;
+		if(!interaction.isChatInputCommand()) return;
 		// check if command is run in DMs.
-		if(interaction.guildId === null || bot.user.id) return;
+		if(interaction.commandGuildId === null || interaction.channel.type === "DM") return;
 
+		// Build Data for command.
 		const cmd = interaction.commandName;
 		const Slashes = bot.slash.get(cmd);
 		if(!Slashes) return;
-		Slashes.run(interaction, bot);
+		// Executhe command.
+		Slashes.execute(interaction, bot);
 	},
 };
